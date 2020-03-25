76dc91d779db6b37ebb364e8c641102a0c478f35
Derek Roman <frank.d.roman@centene.com>
2020-03-25 16:08:08 -0500

    adding header content and bootstrap classes

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
-//import bootstrap from "react-bootstrap"
+
+
+function Hero() {
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  </div>);
+}
+
+function Turn() {
+  return null
+}
+
+function Continue() {
+    return null
+}
+
+function Footer() {
+  return null
+}
 
 function App() {
   return (
-    <div>AuthorQuiz</div>
+    <div className="container-fluid">
+      <Hero/>
+      <Turn/>
+      <Continue />
+      <Footer />
+    </div>
   );
 }
 
